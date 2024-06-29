6bdb9ce7a55477330cde8e1d0b3d4ceb3dd9596f
WilliamVazquez1 <williamvazquezlopez9@gmail.com>
2024-06-29 16:06:41 -0600

    Avance

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>El changarro</title>
-    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
-    <link rel="icon" href="../assets/img/kaiadmin/favicon.ico" type="image/x-icon" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El Changarro - Tienda de Abarrotes</title>
+    <link rel="icon" href="../assets/img/kaiadmin/favicon.ico" type="image/x-icon">
 
     <!-- Fonts and icons -->
     <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
     <script>
         WebFont.load({
-            google: { families: ["Public Sans:300,400,500,600,700"] },
+            google: { families: ["Montserrat:300,400,500,600,700"] },
             custom: {
                 families: [
                     "Font Awesome 5 Solid",
@@ -27,21 +27,104 @@
     </script>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../assets/css/plugins.min.css" />
-    <link rel="stylesheet" href="../assets/css/kaiadmin.min.css" />
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/plugins.min.css">
+    <link rel="stylesheet" href="../assets/css/kaiadmin.min.css">
+    <link rel="stylesheet" href="../assets/css/demo.css">
+
     <style>
-        body.sidebar-collapse .main-panel {
-            margin-left: 80px;
+        /* Custom styles */
+        body {
+            font-family: 'Montserrat', sans-serif;
+            background-color: #f0f0f0;
         }
-        .sidebar-collapse .nav-link {
-            display: none;
+        .sidebar {
+            background-color: #37474f;
+            color: #fff;
+        }
+        .sidebar-logo {
+            padding: 15px 20px;
+            text-align: center;
+        }
+        .sidebar-logo a {
+            color: #fff;
+            text-decoration: none;
+            font-weight: bold;
+            font-size: 1.5rem;
+        }
+        .sidebar-content {
+            padding: 20px;
+        }
+        .nav-secondary .nav-link {
+            color: #ccc;
+        }
+        .nav-secondary .nav-link:hover {
+            color: #fff;
+        }
+        .jumbotron {
+            background-color: #007bff;
+            color: #fff;
+            padding: 100px 20px;
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        .jumbotron h1 {
+            font-size: 3rem;
+            font-weight: bold;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        }
+        .jumbotron p {
+            font-size: 1.5rem;
+            margin-bottom: 30px;
+        }
+        .jumbotron .btn-primary {
+            background-color: #0056b3;
+            border-color: #0056b3;
+            font-size: 1.2rem;
+            padding: 10px 20px;
+        }
+        .jumbotron .btn-primary:hover {
+            background-color: #00408a;
+            border-color: #00408a;
+        }
+        .row {
+            margin-bottom: 30px;
+        }
+        .col-lg-4 {
+            text-align: center;
+            padding: 0 15px;
+        }
+        .col-lg-4 h2 {
+            font-size: 2.5rem;
+            margin-bottom: 20px;
+            color: #007bff;
+        }
+        .col-lg-4 ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        .col-lg-4 ul li {
+            margin-bottom: 10px;
+        }
+        .col-lg-4 blockquote {
+            font-style: italic;
+            border-left: 5px solid #007bff;
+            padding-left: 10px;
+            margin-top: 20px;
+            color: #555;
         }
         .cart {
-            position: absolute;
-            right: 20px;
+            position: fixed;
             top: 20px;
-            cursor: pointer;
+            right: 20px;
+            z-index: 1000;
+            display: flex;
+            align-items: center;
+        }
+        .cart a {
+            color: #ffcc00; /* Color amarillo para el icono del carrito */
+            text-decoration: none;
+            font-size: 1.8rem;
         }
         .cart-content {
             position: fixed;
@@ -77,36 +160,18 @@
         }
         .cart-item-title {
             font-weight: bold;
+            color: #333;
         }
     </style>
- 
-    <link rel="stylesheet" href="../assets/css/demo.css" />
 </head>
 <body>
     <div class="wrapper">
         <!-- Sidebar -->
-        <div class="sidebar" data-background-color="dark">
+        <div class="sidebar">
             <div class="sidebar-logo">
-                <!-- Logo Header -->
-                <div class="logo-header" data-background-color="dark">
-                    <a href="../index.html" class="logo">
-                        <img src="../assets/img/kaiadmin/logo_light.svg" alt="navbar brand" class="navbar-brand" height="20" />
-                    </a>
-                    <button class="navbar-toggler sidenav-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon">
-                            <i class="gg-menu-right"></i>
-                        </span>
-                    </button>
-                    <button class="topbar-toggler more">
-                        <i class="icon-options-vertical"></i>
-                    </button>
-                    <div class="nav-toggle">
-                        <button class="btn btn-toggle toggle-sidebar">
-                            <i class="gg-menu-right"></i>
-                        </button>
-                    </div>
-                </div>
-         
+                <a href="../index.html" class="logo">
+                    <img src="../assets/img/changarro.jpg" alt="El Changarro Logo" height="50">
+                </a>
             </div>
             <div class="sidebar-wrapper scrollbar scrollbar-inner">
                 <div class="sidebar-content">
@@ -124,60 +189,60 @@
                 </div>
             </div>
         </div>
-       
+        
+        <!-- Main Content -->
         <div class="main-panel">
             <main class="main-content">
                 <div class="container-fluid">
-                  
+                    <!-- Cart -->
                     <div class="cart" id="cart">
+                        <a href="{{ url('/register') }}" class="btn btn-primary btn-lg">Regístrate Ahora</a>
                         <a href="#" id="cart-toggle">
-                            <i class="fas fa-shopping-cart"></i>
+                            <i class="fas fa-shopping-cart" style="color: #ffcc00;"></i> <!-- Icono del carrito en color amarillo -->
                             <span id="cart-count">0</span>
                         </a>
                     </div>
 
-                   
+                    <!-- Cart Content -->
                     <div class="cart-content" id="cart-content">
                         <div class="cart-close" id="cart-close">
                             <i class="fas fa-times"></i>
                         </div>
                         <h3>Carrito de Compras</h3>
-                        <div id="cart-items">
-                           
-                        </div>
+                        <div id="cart-items"></div>
                     </div>
 
-                  
-                    <div class="jumbotron" style="background-image: url('{{ asset('img/descarga (3)') }}');">
-                        <h1 class="display-4">Bienvenido a tienda "El changarro"</h1>
-                        <p class="lead">Lo mejor en bebidas y alimentos.</p>
+                    <!-- Dashboard -->
+                    <div class="jumbotron">
+                        <h1 class="display-4">¡Bienvenido a "El Changarro"!</h1>
+                        <p class="lead">Tu tienda favorita de bebidas y alimentos.</p>
                         <hr class="my-4">
-                        <p>Somos una empresa dedicada a la venta de productos abarroteros con la mejor calidad.</p>
-                        <a class="btn btn-primary btn-lg" href="{{ url('/register') }}" role="button">Regístrate Ahora</a>
+                        <p>Somos expertos en ofrecerte productos abarroteros de la más alta calidad.</p>
                     </div>
 
+                    <!-- About, Services, Testimonials -->
                     <div class="row">
                         <div class="col-lg-4">
                             <h2>Sobre Nosotros</h2>
-                            <p>Somos una empresa dedicada a la venta de productos abarroteros con la mejor calidad.</p>
+                            <p>Somos una empresa apasionada por brindar la mejor atención y calidad en nuestros productos.</p>
                         </div>
                         <div class="col-lg-4">
                             <h2>Servicios</h2>
                             <ul>
-                                <li>Servicio de calidad en nuestros productos.</li>
-                                <li>Servicio de calidad hacia nuestros clientes.</li>
+                                <li>Productos frescos y de calidad.</li>
+                                <li>Atención al cliente excepcional.</li>
                             </ul>
                         </div>
                         <div class="col-lg-4">
-                            <h2>Quejas y Sugerencias</h2>
+                            <h2>Testimonios</h2>
                             <blockquote>
-                                <p>"Es una empresa muy dedicada a su trabajo con los mejores productos"</p>
+                                <p>"Siempre me sorprende la dedicación y variedad de productos que ofrecen."</p>
                                 <cite>Cliente Satisfecho</cite>
                             </blockquote>
                         </div>
                     </div>
 
-                
+                    <!-- Products -->
                     <h2>Productos</h2>
                     <div class="row">
                         @forelse($productos as $producto)
@@ -201,6 +266,7 @@
                         @endforelse
                     </div>
 
+                    <!-- Contact Form -->
                     <h2>Contacto</h2>
                     <form action="{{ url('/contact') }}" method="post">
                         @csrf
@@ -218,59 +284,42 @@
                         </div>
                         <button type="submit" class="btn btn-primary">Enviar</button>
                     </form>
-                </div
-            
+                </div>
+            </main>
         </div>
-      
     </div>
 
-
+    <!-- JavaScript -->
     <script src="../assets/js/core/jquery-3.7.1.min.js"></script>
     <script src="../assets/js/core/popper.min.js"></script>
     <script src="../assets/js/core/bootstrap.min.js"></script>
-
-   
     <script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
-
-   
     <script src="../assets/js/plugin/chart.js/chart.min.js"></script>
-
-  
     <script src="../assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
-
     <script src="../assets/js/plugin/chart-circle/circles.min.js"></script>
-
     <script src="../assets/js/plugin/datatables/datatables.min.js"></script>
-
     <script src="../assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
-
-  
     <script src="../assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
     <script src="../assets/js/plugin/jsvectormap/world.js"></script>
-
     <script src="../assets/js/plugin/gmaps/gmaps.js"></script>
-
-  
     <script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
-
     <script src="../assets/js/kaiadmin.min.js"></script>
-
     <script src="../assets/js/setting-demo2.js"></script>
 
     <script>
         $(document).ready(function() {
-          
+            // Toggle cart content
             $('#cart-toggle').on('click', function(e) {
                 e.preventDefault();
                 $('#cart-content').toggleClass('open');
             });
 
-          
+            // Close cart content
             $('#cart-close').on('click', function() {
                 $('#cart-content').removeClass('open');
             });
 
-      
+            // Add to cart functionality
             $('.add-to-cart').on('click', function() {
                 let productId = $(this).data('id');
                 let productName = $(this).closest('.card-body').find('.card-title').text();
@@ -290,5 +339,3 @@
     </script>
 </body>
 </html>
-
-
